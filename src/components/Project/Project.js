609d34fe46e03368b4ffb6f0b67294414f98eac6
Nigel Nkomo <mrnbnkomo@gmail.com>
2023-04-12 12:01:44 +0200

    add arrow icon to project card

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,11 +1,14 @@
 import styles from "./Project.module.css";
+import Arrow from "../Arrow/Arrow";
 
 export default function Project({ imgUrl }) {
   return (
     <section>
       <div className={styles.project}>
         <a href="#" className={styles.projectLink}>
-          ↗
+          <div className={styles.arrow}>
+            <Arrow />
+          </div>
         </a>
         <img src={imgUrl} className={styles.projectImage} />
       </div>
